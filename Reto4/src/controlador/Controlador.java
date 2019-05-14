e1fecb1cd1d70a20a474b7dde124f5af54399e1a
alexl <alexl@DESKTOP-QFR2N0Q>
2019-05-14 19:47:03 +0200

    Creacion del proyecto

---
+++ Reto4/src/controlador/Controlador.java
@@ -0,0 +1,391 @@
+package controlador;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+import modelo.Alojamiento;
+import modelo.Cliente;
+import modelo.Metodos;
+import modelo.Modelo;
+import vista.Vista;
+
+public class Controlador {
+	private static Vista vista;
+	private Modelo modelo;
+	private ArrayList<modelo.Alojamiento> alojamientos;
+	public double Adevolver = 0;
+	public double Introducido = 0;
+	public double preciototal=0;
+	public String  preciotabla="";
+	public String nombrehotel = "";
+	public String nombreubicacion = "";
+
+	public Controlador(Vista vista, Modelo modelo) {
+		Controlador.vista = vista;
+		this.modelo = modelo;
+
+		// Cargamos la pantalla principal
+		vista.mostrarPantalla(vista.getInicio());
+
+		// Cargamos el comboBox con las ciudades
+		
+		initalizeEvents();
+
+	}
+
+	private void initalizeEvents() {
+		
+		vista.getInicio().getBtnseleccionar().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Tras la busqueda inicial vamos a la pantall de seleccion de hoteles
+				if(vista.getInicio().getRdbtnHotel().isSelected()) {
+					rellenarComboCiudades();
+				}else {
+					if(vista.getInicio().getRdbtnApartamento().isSelected()) {
+						rellenarComboCiudadesapartamentos();
+					}else {
+						rellenarComboCiudadescasas();
+						}
+				}
+
+			}
+		});
+		
+		vista.getInicio().getBtn_buscar_inicio().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				/*
+				String mes = Integer.toString(vista.getInicio().getCalendar_entrada().getCalendar().get(Calendar.MONTH) + 1);
+				if(mes>4 && mes<=8) {
+					vista.getInicio2().getTextField().setText("Temporada Alta");
+				}else {
+				}if(mes>8 && mes<=12) {
+					vista.getInicio2().getTextField().setText("Temporada Media");
+				}
+				
+					vista.mostrarPantalla(vista.getInicio2());
+				*/
+							}
+		});
+		vista.getInicio2().getBtnSiguiente().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Tras la busqueda inicial vamos a la pantall de seleccion de hoteles
+				//nombreubicacion = (vista.getInicio().getCombo_ubicacion().getSelectedItem().toString());		
+				//rellenarComboAlojamientos();
+				if(vista.getInicio().getRdbtnHotel().isSelected()) {
+					vista.mostrarPantalla(vista.getListahoteles());
+				
+				//Usamos el metodo que cargara los hoteles de la ubicacion seleccionada
+				DefaultTableModel modelos=modelo.getMetodos().cargarTablahoteles((String) vista.getInicio().getCombo_ubicacion().getSelectedItem());
+				
+				vista.getListahoteles().getTable().setModel(modelos);
+				}else {
+					if(vista.getInicio().getRdbtnApartamento().isSelected()) {
+						vista.mostrarPantalla(vista.getListahoteles());
+						
+						//Usamos el metodo que cargara los hoteles de la ubicacion seleccionada
+						DefaultTableModel modelos=modelo.getMetodos().cargarTablaapartamentos((String) vista.getInicio().getCombo_ubicacion().getSelectedItem());
+						
+						vista.getListahoteles().getTable().setModel(modelos);
+					}else {
+						vista.mostrarPantalla(vista.getListahoteles());
+						
+						//Usamos el metodo que cargara los hoteles de la ubicacion seleccionada
+						DefaultTableModel modelos=modelo.getMetodos().cargarTablacasasrurales((String) vista.getInicio().getCombo_ubicacion().getSelectedItem());
+						
+						vista.getListahoteles().getTable().setModel(modelos);
+						}
+				}
+				
+							}
+		});
+		
+		/*vista.getListahoteles().getBtnAceptar().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Tras la busqueda inicial vamos a la pantall de seleccion de hoteles
+				vista.mostrarPantalla(vista.getListahoteles());
+				rellenarprecios();
+				vista.getListahoteles().getTextField_precio().setText(Double.toString(precioHotel));
+				modelo.getMetodos().modificarfichero();
+			}
+		});
+		
+		
+		vista.getListahoteles().getBtnaceptar_mostrar_lista().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Tras la busqueda inicial vamos a la pantall de seleccion de hoteles
+				nombrehotel = (vista.getListahoteles().getcombo_alojamientos().getSelectedItem().toString());
+				rellenarprecios();
+				vista.getPagar().gettextField().setText(Double.toString(precioHotel));
+				vista.mostrarPantalla(vista.getPagar());
+
+			}
+		});
+*/
+		
+		
+		vista.getListahoteles().getBtnCancelar_mostrar_lista().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Tras la busqueda inicial vamos a la pantall de seleccion de hoteles
+				vista.mostrarPantalla(vista.getInicio());
+
+			}
+		});
+		vista.getListahoteles().getBtnaceptar_mostrar_lista().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Tras la busqueda inicial vamos a la pantall de seleccion de hoteles
+				/*String datotabla = String.valueOf(vista.getListahoteles().getTable().getValueAt(vista.getListahoteles().getTable().getSelectedRow(), 0));
+				vista.mostrarPantalla(vista.getHotel_seleccionado());
+				vista.getHotel_seleccionado().gettF_nombre_Hotel().setText(datotabla);*/
+				vista.mostrarPantalla(vista.getInicio_sesion());
+			}
+		});
+		vista.getPagar().getbtnCancelar().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Tras la busqueda inicial vamos a la pantall de seleccion de hoteles
+				vista.mostrarPantalla(vista.getInicio());
+
+			}
+		});
+		
+		vista.getHotel_seleccionado().getBtnReservar_Hotel().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Tras la busqueda inicial vamos a la pantall de seleccion de hoteles
+				//vista.mostrarPantalla(vista.getInicio_sesion());
+
+			}
+		});
+		
+		
+		
+	
+	
+		
+		vista.getInicio_sesion().getBtnAceptar_Inicio_Sesion().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Tras la busqueda inicial vamos a la pantall de seleccion de hoteles
+				  try
+	                {                    
+	                    //chekar si el usuario escrbio el nombre de usuario y pw
+	                    if (vista.getInicio_sesion().getTf_Usuario_Inicio_Sesion().getText().length() > 0 && vista.getInicio_sesion().getPf_Contra_Inicio_Sesion().getText().length() > 0 )
+	                    {
+	                    	String contrasena = modelo.getMetodos().encriptarPass(vista.getInicio_sesion().getPf_Contra_Inicio_Sesion().getText());
+	                        // Si el usuario si fue validado correctamente
+	                    	System.out.println(contrasena);
+	                        if( modelo.getMetodos().comprobarLogin( vista.getInicio_sesion().getTf_Usuario_Inicio_Sesion().getText(), vista.getInicio_sesion().getPf_Contra_Inicio_Sesion().getText() ) )    //enviar datos a validar
+	                        {
+	                            // Codigo para mostrar la ventana principal
+	                        	 preciotabla = String.valueOf(vista.getListahoteles().getTable().getValueAt(vista.getListahoteles().getTable().getSelectedRow(), 1));
+	            				 preciototal = Double.parseDouble(preciotabla);
+	            				 vista.mostrarPantalla(vista.getPagar());
+	            				vista.getPagar().gettextField().setText(preciotabla);
+
+	 
+	                        }
+	                        else
+	                        {
+	                            JOptionPane.showMessageDialog(null, "El nombre de usuario y/o contrasenia no son validos.");
+	                            JOptionPane.showMessageDialog(null, vista.getInicio_sesion().getTf_Usuario_Inicio_Sesion().getText()+" " +vista.getInicio_sesion().getPf_Contra_Inicio_Sesion().getText() );
+	                            vista.getInicio_sesion().getTf_Usuario_Inicio_Sesion().setText("");    //limpiar campos
+	                            vista.getInicio_sesion().getPf_Contra_Inicio_Sesion().setText("");        
+	                             
+	                            vista.getInicio_sesion().getTf_Usuario_Inicio_Sesion().requestFocusInWindow();
+	                        }
+	 
+	                    }
+	                    else
+	                    {
+	                        JOptionPane.showMessageDialog(null, "Debe escribir nombre de usuario y contrasenia.\n" +
+	                            "NO puede dejar ningun campo vacio");
+	                    }
+	 
+	                } catch (Exception e)
+	                {
+	                    e.printStackTrace();
+	                }
+
+			}
+		});
+		
+		vista.getInicio_sesion().getBtnCancelar_Inicio_Sesion().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Tras la busqueda inicial vamos a la pantall de seleccion de hoteles
+				vista.mostrarPantalla(vista.getInicio());
+
+			}
+		});
+		
+		
+		vista.getHotel_seleccionado().getBtnCancelar_Hotel().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Tras la busqueda inicial vamos a la pantall de seleccion de hoteles
+				vista.mostrarPantalla(vista.getInicio());
+
+			}
+		});
+		
+		vista.getPagar().getbtn50euro().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Introducimos 50 eurazos
+				SumarIntroducido(50);
+			}
+		});
+		vista.getPagar().getbtn20euro().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Introducimos 20 eurazos
+				SumarIntroducido(20);
+			}
+		});
+		vista.getPagar().getbtn10euro().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Introducimos 10 eurazos
+				SumarIntroducido(10);
+			}
+		});
+		vista.getPagar().getbtn5euro().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Introducimos 5 eurazos
+				SumarIntroducido(5);
+			}
+		});
+		vista.getPagar().getbtn2euro().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Introducimos 2 eurazos
+				SumarIntroducido(2);
+			}
+		});
+		vista.getPagar().getbtn1euro().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Introducimos 1 eurazo
+				SumarIntroducido(1);
+			}
+		});
+		vista.getPagar().getbtn50cnt().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Introducimos 50 centimazos
+				SumarIntroducido(0.50);
+			}
+		});
+		vista.getPagar().getbtn20cnt().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Introducimos 20 centimazos
+				SumarIntroducido(0.20);
+			}
+		});
+		vista.getPagar().getbtn10cnt().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Introducimos 51 centimazos
+				SumarIntroducido(0.10);
+			}
+		});
+		vista.getPagar().getbtn5cnt().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Introducimos 5 centimazos
+				SumarIntroducido(0.05);
+			}
+		});
+		vista.getPagar().getbtn2cnt().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Introducimos 2 centimazos
+				SumarIntroducido(0.02);
+			}
+		});
+		vista.getPagar().getbtn1cnt().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				SumarIntroducido(0.01);
+			}
+				});
+			}
+	
+
+
+
+	// Metodo para rellenar el combobox con los datos consultados a la BBDD
+
+	private void rellenarComboCiudades() {
+		// Sacar las lineas de la BBDD y rellenar el combobox
+		// 1.Sacar datos de la BBDD
+
+		ArrayList<String> ciudad = modelo.getMetodos().cargarciudades();
+
+		// 2.Rellenar combo de lineas
+
+		for (int i = 0; i < ciudad.size(); i++) {
+			vista.getInicio().getCombo_ubicacion().addItem(ciudad.get(i));
+		}
+
+	}
+	private void rellenarComboCiudadesapartamentos() {
+		// Sacar las lineas de la BBDD y rellenar el combobox
+		// 1.Sacar datos de la BBDD
+
+		ArrayList<String> ciudad = modelo.getMetodos().cargarciudadesapartamentos();
+
+		// 2.Rellenar combo de lineas
+
+		for (int i = 0; i < ciudad.size(); i++) {
+			vista.getInicio().getCombo_ubicacion().addItem(ciudad.get(i));
+		}
+
+	}
+	
+	private void rellenarComboCiudadescasas() {
+		// Sacar las lineas de la BBDD y rellenar el combobox
+		// 1.Sacar datos de la BBDD
+
+		ArrayList<String> ciudad = modelo.getMetodos().cargarciudadesCasas();
+
+		// 2.Rellenar combo de lineas
+
+		for (int i = 0; i < ciudad.size(); i++) {
+			vista.getInicio().getCombo_ubicacion().addItem(ciudad.get(i));
+		}
+
+	}
+	private void confirmarlogin() {
+		String user=vista.getInicio_sesion().getTf_Usuario_Inicio_Sesion().getText();
+		String password=String.valueOf(vista.getInicio_sesion().getPf_Contra_Inicio_Sesion().getPassword());
+		
+		boolean validarLogin=modelo.getMetodos().comprobarLogin(user, password);
+		if(validarLogin==true) {
+		
+			vista.mostrarPantalla(vista.getPagar());
+			
+		}else {
+			//mostramos el error pero borramos los campos para hacer un intendo de nuevo
+			System.out.println("esta mal");
+			vista.mostrarPantalla(vista.getInicio_sesion());
+		}
+	}
+	private void rellenarresumendatos() {
+		
+	}
+	
+
+	
+	
+	public void SumarIntroducido(double cantidad) {
+		Introducido=Introducido+cantidad;
+		vista.getPagar().gettextField_1().setText(Double.toString(Introducido));
+		//Datos.sacarResto=Datos.Total-Datos.TotalIntroducido;
+		if(preciototal > Introducido) {
+			vista.getPagar().gettextField_2().setText(Double.toString(0));
+			
+		}else {
+			Adevolver=Introducido-preciototal;
+					;
+			vista.getPagar().gettextField_2().setText(Double.toString(Adevolver));
+			
+		}
+	}
+
+
+}
---
+++ Reto4/src/modelo/Metodos.java
@@ -0,0 +1,443 @@
+package modelo;
+
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import javax.swing.table.DefaultTableModel;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+import vista.Vista;
+
+public class Metodos {
+
+	private static ArrayList<String> ciudades;
+	private static ArrayList<String> hoteles;
+	private static ArrayList<String> apartamentos;
+	private static ArrayList<String> casas;
+	private static ArrayList<String> precios;
+	private ArrayList<modelo.Alojamiento> busquedas;
+	static String fecha;
+	static String fecha2;
+	// metodo para guardar el nombre de las ciudades en un arraylist con el que
+	// cargaremos el combobox
+
+	public ArrayList<String> cargarciudades() {
+		ciudades = new ArrayList<String>();
+		String sql = "SELECT ubicacion FROM hoteles";
+		BBDD conectar = new BBDD();
+		try {
+			PreparedStatement ps = conectar.conectarBase().prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+
+				ciudades.add(rs.getString(1));
+
+			}
+			return ciudades;
+		} catch (SQLException e) {
+			System.err.println("Consulta erronea, motivo del error: " + e);
+			return ciudades = null;
+		}
+
+	}
+
+	public ArrayList<String> cargarciudadesCasas() {
+		casas = new ArrayList<String>();
+		String sql = "SELECT ubicacion FROM casarural";
+		BBDD conectar = new BBDD();
+		try {
+			PreparedStatement ps = conectar.conectarBase().prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+
+				casas.add(rs.getString(1));
+
+			}
+			return casas;
+		} catch (SQLException e) {
+			System.err.println("Consulta erronea, motivo del error: " + e);
+			return casas = null;
+		}
+
+	}
+
+	public ArrayList<String> cargarciudadesapartamentos() {
+		apartamentos = new ArrayList<String>();
+		String sql = "SELECT ubicacion FROM apartamentos";
+		BBDD conectar = new BBDD();
+		try {
+			PreparedStatement ps = conectar.conectarBase().prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+
+				apartamentos.add(rs.getString(1));
+
+			}
+			return apartamentos;
+		} catch (SQLException e) {
+			System.err.println("Consulta erronea, motivo del error: " + e);
+			return apartamentos = null;
+		}
+
+	}
+	/*
+	public ArrayList<String> cargarCiudades() {
+
+		Vista vista = new Vista();
+		Modelo modelo = new Modelo();
+		ArrayList<String> ciudades = new ArrayList<String>();
+		String sql = " ";
+
+		// Comprobamos el alojamiento seleccionado para cargar las ciudades
+		if (vista.getInicio().getRdbtnHotel().isSelected() == true) {
+			sql = "SELECT DISTINCT ubicacion FROM hoteles";
+		} else if (vista.getInicio().getRdbtnApartamento().isSelected() == true) {
+			sql = "SELECT DISTINCT ubicacion FROM apartamentos";
+		} else if (vista.getInicio().getRdbtnCasaRural().isSelected() == true) {
+			sql = "SELECT DISTINCT ubicacion FROM casarural";
+		} else {
+			System.err.println("No se ha seleccionado el tipo de alojamiento");
+		}
+
+		try {
+			modelo.getBasededatos().conectarBase();
+
+			PreparedStatement ps = modelo.getBasededatos().conectarBase().prepareStatement(sql);
+
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+				ciudades.add(rs.getString(1));
+			}
+
+		} catch (SQLException e) {
+			System.err.println("Fallo en la conexion, motivo del error: \n" + e);
+		}
+
+		return ciudades;
+
+	}*/
+
+	public ArrayList<String> cargarApartamentos(String nombreubicacion) {
+		Vista vista = new Vista();
+
+		BBDD conectar = new BBDD();
+		apartamentos = new ArrayList<String>();
+		String sql = "SELECT nombre FROM apartamentos WHERE ubicacion LIKE '" + nombreubicacion + "' ";
+
+		try {
+			PreparedStatement ps = conectar.conectarBase().prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+				apartamentos.add(rs.getString(1));
+
+			}
+			return apartamentos;
+		} catch (Exception e) {
+			System.err.println("Consulta no valida");
+			return apartamentos = null;
+		}
+
+	}
+
+	public ArrayList<String> cargarcasas(String nombreubicacion) {
+		Vista vista = new Vista();
+
+		BBDD conectar = new BBDD();
+		casas = new ArrayList<String>();
+		String sql = "SELECT nombre FROM casarural WHERE ubicacion LIKE '" + nombreubicacion + "' ";
+
+		try {
+			PreparedStatement ps = conectar.conectarBase().prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+				casas.add(rs.getString(1));
+
+			}
+			return casas;
+		} catch (Exception e) {
+			System.err.println("Consulta no valida");
+			return casas = null;
+		}
+
+	}
+
+	public ArrayList<String> cargarHoteles(String nombreubicacion) {
+		Vista vista = new Vista();
+
+		BBDD conectar = new BBDD();
+		hoteles = new ArrayList<String>();
+		String sql = "SELECT nombre FROM hoteles WHERE ubicacion LIKE '" + nombreubicacion + "' ";
+
+		try {
+			PreparedStatement ps = conectar.conectarBase().prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+				hoteles.add(rs.getString(1));
+
+			}
+			return hoteles;
+		} catch (Exception e) {
+			System.err.println("Consulta no valida");
+			return hoteles = null;
+		}
+
+	}
+
+	// Metodo para buscar alojamiento segun los valores indicados en la busqueda
+	public ArrayList<Alojamiento> buscarAlojamientos() {
+		ArrayList<Alojamiento> ret = new ArrayList<Alojamiento>();
+
+		Vista vista = new Vista();
+		BBDD conectar = new BBDD();
+		String sql = "SELECT * FROM hoteles WHERE ubicacion LIKE '" + vista.getInicio().getCombo_ubicacion() + "'";
+		Alojamiento alojamiento = new Alojamiento();
+		try {
+			if (vista.getInicio().getCombo_ubicacion().getSelectedItem() != null) {
+				PreparedStatement ps = conectar.conectarBase().prepareStatement(sql);
+				ResultSet rs = ps.executeQuery();
+				while (rs.next()) {
+
+					alojamiento.setNum_habitaciones(rs.getInt(1));
+					alojamiento.setNum_camas(rs.getInt(2));
+					alojamiento.setTipo_cama(rs.getString(3));
+					alojamiento.setPension(rs.getString(4));
+					alojamiento.setUbicacion(rs.getString(5));
+					alojamiento.setNombre(rs.getString(6));
+					ret.add(alojamiento);
+				}
+				rs.close();
+				System.out.println("Lista cargada");
+			} else {
+				System.err.println("Error al cargar la lista, Ubicacion nula");
+			}
+
+		} catch (SQLException e) {
+			System.err.println("Conexion fallida, causa del error: " + e);
+		}
+
+		return ret;
+
+	}
+
+	public DefaultTableModel cargarTablahoteles(String ubicacion) {
+
+		Vista vista = new Vista();
+
+		String sql = "SELECT nombre,precio,tipo_cama,n_camas,n_habitaciones FROM hoteles WHERE ubicacion LIKE '"
+				+ ubicacion + "'";
+		BBDD conectar = new BBDD();
+		busquedas = null;
+
+		DefaultTableModel modelo = new DefaultTableModel();
+
+		modelo.addColumn("Nombre: ");
+		modelo.addColumn("Precio: ");
+		modelo.addColumn("Tipo Cama: ");
+		modelo.addColumn("Nº Camas: ");
+		modelo.addColumn("Nº Habitaciones: ");
+
+		try {
+
+			PreparedStatement ps = conectar.conectarBase().prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+
+				// Creamos un array para carda fila de la tabla
+				Object[] fila = new Object[5]; // Hay 5 columnas en la tabla asi que asignamos 5 posiciones
+
+				// insertamos los datos en su posicion del array
+				for (int i = 0; i < fila.length; i++) {
+					fila[i] = rs.getObject(i + 1);
+				}
+
+				// anyadimos la fila a la tabla
+				modelo.addRow(fila);
+
+			}
+		} catch (SQLException e) {
+			System.err.println("Conexion fallida, causa del error: " + e);
+		}
+
+		// pasamos la tabla poblada lista para usar
+		return modelo;
+
+	}
+
+	public DefaultTableModel cargarTablaapartamentos(String ubicacion) {
+
+		Vista vista = new Vista();
+
+		String sql = "SELECT nombre,precio,numerobanos,mcuadrados FROM apartamentos WHERE ubicacion LIKE '" + ubicacion
+				+ "'";
+		BBDD conectar = new BBDD();
+		busquedas = null;
+
+		DefaultTableModel modelo = new DefaultTableModel();
+
+		modelo.addColumn("Nombre: ");
+		modelo.addColumn("Precio: ");
+		modelo.addColumn("Numerobanos: ");
+		modelo.addColumn("Mcuadrados: ");
+
+		try {
+
+			PreparedStatement ps = conectar.conectarBase().prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+
+				// Creamos un array para carda fila de la tabla
+				Object[] fila = new Object[4]; // Hay 5 columnas en la tabla asi que asignamos 5 posiciones
+
+				// insertamos los datos en su posicion del array
+				for (int i = 0; i < fila.length; i++) {
+					fila[i] = rs.getObject(i + 1);
+				}
+
+				// anyadimos la fila a la tabla
+				modelo.addRow(fila);
+
+			}
+		} catch (SQLException e) {
+			System.err.println("Conexion fallida, causa del error: " + e);
+		}
+
+		// pasamos la tabla poblada lista para usar
+		return modelo;
+
+	}
+
+	public DefaultTableModel cargarTablacasasrurales(String ubicacion) {
+
+		Vista vista = new Vista();
+
+		String sql = "SELECT nombre,precio,numerohabita,mcuadrados FROM apartamentos WHERE casarural LIKE '" + ubicacion
+				+ "'";
+		BBDD conectar = new BBDD();
+		busquedas = null;
+
+		DefaultTableModel modelo = new DefaultTableModel();
+
+		modelo.addColumn("Nombre: ");
+		modelo.addColumn("Precio: ");
+		modelo.addColumn("Numerohabita ");
+		modelo.addColumn("Mcuadrados ");
+
+		try {
+
+			PreparedStatement ps = conectar.conectarBase().prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+
+				// Creamos un array para carda fila de la tabla
+				Object[] fila = new Object[4]; // Hay 5 columnas en la tabla asi que asignamos 5 posiciones
+
+				// insertamos los datos en su posicion del array
+				for (int i = 0; i < fila.length; i++) {
+					fila[i] = rs.getObject(i + 1);
+				}
+
+				// anyadimos la fila a la tabla
+				modelo.addRow(fila);
+
+			}
+		} catch (SQLException e) {
+			System.err.println("Conexion fallida, causa del error: " + e);
+		}
+
+		// pasamos la tabla poblada lista para usar
+		return modelo;
+
+	}
+
+	public static void imprimirfecha() {
+		Vista vista = new Vista();
+		String dia = Integer.toString(vista.getInicio().getCalendar_entrada().getCalendar().get(Calendar.DAY_OF_MONTH));
+		String mes = Integer.toString(vista.getInicio().getCalendar_entrada().getCalendar().get(Calendar.MONTH) + 1);
+		String year = Integer.toString(vista.getInicio().getCalendar_entrada().getCalendar().get(Calendar.YEAR));
+		fecha = (dia + "-" + mes + "-" + year);
+
+		String dia2 = Integer.toString(vista.getInicio().getCalendar_salida().getCalendar().get(Calendar.DAY_OF_MONTH));
+		String mes2 = Integer.toString(vista.getInicio().getCalendar_salida().getCalendar().get(Calendar.MONTH) + 1);
+		String year2 = Integer.toString(vista.getInicio().getCalendar_salida().getCalendar().get(Calendar.YEAR));
+		fecha2 = (dia2 + "-" + mes2 + "-" + year2);
+
+	}
+
+	public boolean comprobarLogin(String dni, String pass) {
+		pass = encriptarPass(pass);
+		BBDD conectar = new BBDD();
+		String sql = "SELECT Nombreusu,Contrasena FROM usuarios WHERE Nombreusu LIKE '" + dni
+				+ "' AND Contrasena LIKE '" + pass + "'";
+
+		try {
+			PreparedStatement ps = conectar.conectarBase().prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+
+			rs.last();
+			if (rs.first()) // si es valido el primer reg. hay una fila, tons el usuario y su pw existen
+				return true; // usuario validado correctamente
+			else
+				return false; // usuario validado incorrectamente
+
+		} catch (Exception e) {
+			System.err.println("Consulta erronea" + e);
+			return false;
+		}
+	}
+
+	/*
+	 * 25d55ad283aa400af464c76d713c07ad (12345678) e919c49d5f0cd737285367810a3394d0
+	 * (hotel) 81dc9bdb52d04dc20036dbd8313ed055 (1234)
+	 * 
+	 */
+	public String encriptarPass(String pass) {
+
+		pass = DigestUtils.md5Hex(pass);
+		return pass;
+	}
+
+	// Metodo para generar un fichero de texto
+	public static void modificarfichero() {
+		FileWriter fichero = null;
+		PrintWriter pw = null;
+		try {
+			fichero = new FileWriter("C:\\Users\\alain\\Desktop\\prueba.txt");
+			pw = new PrintWriter(fichero);
+
+			imprimirfecha();
+
+			pw.println(fecha);
+			pw.println(fecha2);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				// Nuevamente aprovechamos el finally para
+				// asegurarnos que se cierra el fichero.
+				if (null != fichero)
+					fichero.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+
+	}
+
+}
